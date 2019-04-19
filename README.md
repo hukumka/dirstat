c5a7cc469a25272c337e87b74bae2937d66ebfe8
hukumka <hukumka212@gmail.com>
2019-04-19 09:18:14 +0000

    Update docs

--- README.md
+++ README.md
@@ -4,16 +4,19 @@
 
 Simple utility to display total size of all files in directory
 
-USAGE:
-   dirstat [FLAGS] <input_dir>
+Usage:
 
-FLAGS:
-    -h, --help       Prints help information
-    -n, --nice       Display result in readable units
-    -V, --version    Prints version information
+`dirstat [FLAGS] <input_dir>`
 
-ARGS:
-    <input_dir>    Path to file or directory you want check stats on
+Flags
+|:----------|------------------------:|
+|-h, --help | Prints help information |
+|-n, --nice | Display result in readable units |
+|-V, --version | Prints version information |
+
+Args 
+|:----------|------------------------:|
+| <input_dir> | Path to file or directory you want check stats on |
 
 
 <!-- cargo-sync-readme end -->
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 //! Simple utility to display total size of all files in directory
 //!
-//! USAGE:
-//!    dirstat [FLAGS] <input_dir>
-//!
-//! FLAGS:
-//!     -h, --help       Prints help information
-//!     -n, --nice       Display result in readable units
-//!     -V, --version    Prints version information
+//! Usage:
 //! 
-//! ARGS:
-//!     <input_dir>    Path to file or directory you want check stats on
+//! `dirstat [FLAGS] <input_dir>`
+//!
+//! Flags
+//! |:----------|------------------------:|
+//! |-h, --help | Prints help information |
+//! |-n, --nice | Display result in readable units |
+//! |-V, --version | Prints version information |
+//!
+//! Args 
+//! |:----------|------------------------:|
+//! | <input_dir> | Path to file or directory you want check stats on |
 //!
 
 use structopt::StructOpt;
